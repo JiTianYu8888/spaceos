cd3b734334ae1a5308a58164d185f2da57ce9f8a
JiTianYu8888 <153918990+JiTianYu8888@users.noreply.github.com>
2023-12-15 21:03:18 +0800

    Add files via upload

---
+++ haribote/bootpack.h
@@ -0,0 +1,424 @@
+/* asmhead.nas */
+struct BOOTINFO { /* 0x0ff0-0x0fff */
+	char cyls; /* ƒu[ƒgƒZƒNƒ^‚Í‚ÁE��±‚Ü‚ÁE�fƒBƒXƒN‚ð“ÁE��ñ‚¾‚Ì‚© */
+	char leds; /* ƒu[ƒgŽž‚ÌƒL[ƒ{[ƒh‚ÌLED‚Ìó‘ÁE*/
+	char vmode; /* ƒrƒfƒIƒ‚[ƒh  ‰½ƒrƒbƒgƒJƒ‰[‚© */
+	char reserve;
+	short scrnx, scrny; /* ‰æ–Ê‰ð‘œ“x */
+	char *vram;
+};
+#define ADR_BOOTINFO	0x00000ff0
+#define ADR_DISKIMG		0x00100000
+
+/* naskfunc.nas */
+void io_hlt(void);
+void io_cli(void);
+void io_sti(void);
+void io_stihlt(void);
+int io_in8(int port);
+void io_out8(int port, int data);
+int io_in16(int port);
+void io_out16(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+int load_cr0(void);
+void store_cr0(int cr0);
+void load_tr(int tr);
+void asm_inthandler0c(void);
+void asm_inthandler0d(void);
+void asm_inthandler20(void);
+void asm_inthandler21(void);
+void asm_inthandler2c(void);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+void farjmp(int eip, int cs);
+void farcall(int eip, int cs);
+void asm_hrb_api(void);
+void start_app(int eip, int cs, int esp, int ds, int *tss_esp0);
+void asm_end_app(void);
+
+/* fifo.c */
+struct FIFO32 {
+	int *buf;
+	int p, q, size, free, flags;
+	struct TASK *task;
+};
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task);
+int fifo32_put(struct FIFO32 *fifo, int data);
+int fifo32_get(struct FIFO32 *fifo);
+int fifo32_status(struct FIFO32 *fifo);
+
+/* graphic.c */
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen8(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize);
+    int read_picture(int *fat, char *vram, int x, int y); /* ?���I */	
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
+/* dsctbl.c */
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+#define ADR_IDT			0x0026f800
+#define LIMIT_IDT		0x000007ff
+#define ADR_GDT			0x00270000
+#define LIMIT_GDT		0x0000ffff
+#define ADR_BOTPAK		0x00280000
+#define LIMIT_BOTPAK	0x0007ffff
+#define AR_DATA32_RW	0x4092
+#define AR_CODE32_ER	0x409a
+#define AR_LDT			0x0082
+#define AR_TSS32		0x0089
+#define AR_INTGATE32	0x008e
+
+/* int.c */
+void init_pic(void);
+#define PIC0_ICW1		0x0020
+#define PIC0_OCW2		0x0020
+#define PIC0_IMR		0x0021
+#define PIC0_ICW2		0x0021
+#define PIC0_ICW3		0x0021
+#define PIC0_ICW4		0x0021
+#define PIC1_ICW1		0x00a0
+#define PIC1_OCW2		0x00a0
+#define PIC1_IMR		0x00a1
+#define PIC1_ICW2		0x00a1
+#define PIC1_ICW3		0x00a1
+#define PIC1_ICW4		0x00a1
+
+/* keyboard.c */
+void inthandler21(int *esp);
+void wait_KBC_sendready(void);
+void init_keyboard(struct FIFO32 *fifo, int data0);
+#define PORT_KEYDAT		0x0060
+#define PORT_KEYCMD		0x0064
+
+/* mouse.c */
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+void inthandler2c(int *esp);
+void enable_mouse(struct FIFO32 *fifo, int data0, struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+
+/* memory.c */
+#define MEMMAN_FREES		4090	/* ‚±‚ê‚ÁE��ñ32KB */
+#define MEMMAN_ADDR			0x003c0000
+struct FREEINFO {	/* ‚ ‚«î•ñ */
+	unsigned int addr, size;
+};
+struct MEMMAN {		/* ƒE�‚ƒŠŠÁE��E*/
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
+/* sheet.c */
+#define MAX_SHEETS		256
+struct SHEET {
+	unsigned char *buf;
+	int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+	struct SHTCTL *ctl;
+	struct TASK *task;
+};
+struct SHTCTL {
+	unsigned char *vram, *map;
+	int xsize, ysize, top;
+	struct SHEET *sheets[MAX_SHEETS];
+	struct SHEET sheets0[MAX_SHEETS];
+};
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
+struct SHEET *sheet_alloc(struct SHTCTL *ctl);
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
+void sheet_updown(struct SHEET *sht, int height);
+void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1);
+void sheet_slide(struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHEET *sht);
+
+/* timer.c */
+#define MAX_TIMER		500
+struct TIMER {
+	struct TIMER *next;
+	unsigned int timeout;
+	char flags, flags2;
+	struct FIFO32 *fifo;
+	int data;
+};
+struct TIMERCTL {
+	unsigned int count, next;
+	struct TIMER *t0;
+	struct TIMER timers0[MAX_TIMER];
+};
+extern struct TIMERCTL timerctl;
+void init_pit(void);
+struct TIMER *timer_alloc(void);
+void timer_free(struct TIMER *timer);
+void timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data);
+void timer_settime(struct TIMER *timer, unsigned int timeout);
+void inthandler20(int *esp);
+int timer_cancel(struct TIMER *timer);
+void timer_cancelall(struct FIFO32 *fifo);
+
+/* mtask.c */
+#define MAX_TASKS		1000	/* ÁE��åƒ^ƒXƒN E*/
+#define TASK_GDT0		3		/* TSS‚ðGDT‚Ì‰½”Ô‚©‚çŠ„‚è“–‚ÁE��é‚Ì‚© */
+#define MAX_TASKS_LV	100
+#define MAX_TASKLEVELS	10
+struct TSS32 {
+	int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
+	int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	int es, cs, ss, ds, fs, gs;
+	int ldtr, iomap;
+};
+struct TASK {
+	int sel, flags; /* sel‚ÍGDT‚Ì”Ô†‚Ì‚±‚ÁE*/
+	int level, priority;
+	struct FIFO32 fifo;
+	struct TSS32 tss;
+	struct SEGMENT_DESCRIPTOR ldt[2];
+	struct CONSOLE *cons;
+	int ds_base, cons_stack;
+	struct FILEHANDLE *fhandle;
+	int *fat;
+	char *cmdline;
+	unsigned char langmode, langbyte1;
+};
+struct TASKLEVEL {
+	int running; /* “®ì‚µ‚ÁE��¢‚éƒ^ƒXƒN‚Ì E*/
+	int now; /* Œ»Ý“®ì‚µ‚ÁE��¢‚éƒ^ƒXƒN‚ª‚ÁE��ê‚¾‚©•ª‚©‚é‚æ‚¤‚É‚·‚é‚½‚ß‚Ì•Ï E*/
+	struct TASK *tasks[MAX_TASKS_LV];
+};
+struct TASKCTL {
+	int now_lv; /* Œ»Ý“®ì’†‚ÌƒŒƒxƒ‹ */
+	char lv_change; /* ŽŸ‰ñƒ^ƒXƒNƒXƒCƒbƒ`‚Ì‚ÁE��«‚ÉAƒŒƒxƒ‹‚à•Ï‚¦‚½‚Ù‚¤‚ª‚¢‚¢‚©‚ÁE��¤‚© */
+	struct TASKLEVEL level[MAX_TASKLEVELS];
+	struct TASK tasks0[MAX_TASKS];
+};
+extern struct TASKCTL *taskctl;
+extern struct TIMER *task_timer;
+struct TASK *task_now(void);
+struct TASK *task_init(struct MEMMAN *memman);
+struct TASK *task_alloc(void);
+void task_run(struct TASK *task, int level, int priority);
+void task_switch(void);
+void task_sleep(struct TASK *task);
+
+/* window.c */
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
+void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
+void change_wtitle8(struct SHEET *sht, char act);
+
+/* console.c */
+struct CONSOLE {
+	struct SHEET *sht;
+	int cur_x, cur_y, cur_c;
+	struct TIMER *timer;
+};
+struct FILEHANDLE {
+	char *buf;
+	int size;
+	int pos;
+};
+void console_task(struct SHEET *sheet, int memtotal);
+void cons_putchar(struct CONSOLE *cons, int chr, char move);
+void cons_newline(struct CONSOLE *cons);
+void cons_putstr0(struct CONSOLE *cons, char *s);
+void cons_putstr1(struct CONSOLE *cons, char *s, int l);
+void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, int memtotal);
+void cmd_mem(struct CONSOLE *cons, int memtotal);
+void cmd_cls(struct CONSOLE *cons);
+void cmd_dir(struct CONSOLE *cons);
+void cmd_exit(struct CONSOLE *cons, int *fat);
+void cmd_start(struct CONSOLE *cons, char *cmdline, int memtotal);
+void cmd_ncst(struct CONSOLE *cons, char *cmdline, int memtotal);
+void cmd_langmode(struct CONSOLE *cons, char *cmdline);
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
+int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
+int *inthandler0d(int *esp);
+int *inthandler0c(int *esp);
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col);
+
+/* file.c */
+struct FILEINFO {
+	unsigned char name[8], ext[3], type;
+	char reserve[10];
+	unsigned short time, date, clustno;
+	unsigned int size;
+};
+void file_readfat(int *fat, unsigned char *img);
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
+struct FILEINFO *file_search(char *name, struct FILEINFO *finfo, int max);
+
+/* bootpack.c */
+struct TASK *open_constask(struct SHEET *sht, unsigned int memtotal);
+struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal);
+
+/**CMOS����[��**/
+#define cmos_index 0x70
+#define cmos_data 0x71
+/**CMOS��?�M���Έ�*/
+#define CMOS_CUR_SEC	0x0			//CMOS���O�b(BCD)
+#define CMOS_ALA_SEC	0x1			//CMOS?�x�b(BCD)
+#define CMOS_CUR_MIN	0x2			//CMOS���O��(BCD)
+#define CMOS_ALA_MIN	0x3			//CMOS?�x��(BCD)
+#define CMOS_CUR_HOUR	0x4			//CMOS���O?(BCD)
+#define CMOS_ALA_HOUR	0x5			//CMOS?�x?(BCD)
+#define CMOS_WEEK_DAY	0x6			//CMOS������O�V(BCD)
+#define CMOS_MON_DAY	0x7			//CMOS�ꌎ���O��(BCD)
+#define CMOS_CUR_MON	0x8			//CMOS���O��(BCD)
+#define CMOS_CUR_YEAR	0x9			//CMOS���O�N(BCD)
+#define CMOS_DEV_TYPE	0x12		//CMOS??��i��
+#define CMOS_CUR_CEN	0x32		//CMOS���O��?(BCD)
+
+#define BCD_HEX(n)	((n >> 4) * 10) + (n & 0xf)  //BCD?�\�Z?��
+
+#define BCD_ASCII_first(n)	(((n<<4)>>4)+0x30)
+#define BCD_ASCII_S(n)	((n<<4)+0x30)
+
+unsigned int get_hour_hex();
+unsigned int get_min_hex();
+unsigned int get_sec_hex();
+unsigned int get_day_of_month();
+unsigned int get_day_of_week();
+unsigned int get_mon_hex();
+unsigned int get_year();
+
+/* acpi.c */
+struct ACPI_RSDP {
+	char Signature[8];
+	unsigned char Checksum;
+	char OEMID[6];
+	unsigned char Revision;
+	unsigned int RsdtAddress;
+	unsigned int Length;
+	unsigned int XsdtAddress[2];
+	unsigned char ExtendedChecksum;
+	unsigned char Reserved[3];
+};
+
+struct ACPISDTHeader {
+  char Signature[4];
+  unsigned int Length;
+  unsigned char Revision;
+  unsigned char Checksum;
+  char OEMID[6];
+  char OEMTableID[8];
+  unsigned int OEMRevision;
+  unsigned int CreatorID;
+  unsigned int CreatorRevision;
+};
+
+struct ACPI_RSDT {
+	struct ACPISDTHeader header;
+	unsigned int Entry;
+};
+
+typedef struct
+{
+  unsigned char AddressSpace;
+  unsigned char BitWidth;
+  unsigned char BitOffset;
+  unsigned char AccessSize;
+  unsigned int Address[2];
+} GenericAddressStructure;
+
+struct ACPI_FADT
+{
+    struct   ACPISDTHeader h;
+    unsigned int FirmwareCtrl;
+    unsigned int Dsdt;
+ 
+    // field used in ACPI 1.0; no longer in use, for compatibility only
+    unsigned char  Reserved;
+ 
+    unsigned char  PreferredPowerManagementProfile;
+    unsigned short SCI_Interrupt;
+    unsigned int SMI_CommandPort;
+    unsigned char  AcpiEnable;
+    unsigned char  AcpiDisable;
+    unsigned char  S4BIOS_REQ;
+    unsigned char  PSTATE_Control;
+    unsigned int PM1aEventBlock;
+    unsigned int PM1bEventBlock;
+    unsigned int PM1aControlBlock;
+    unsigned int PM1bControlBlock;
+    unsigned int PM2ControlBlock;
+    unsigned int PMTimerBlock;
+    unsigned int GPE0Block;
+    unsigned int GPE1Block;
+    unsigned char  PM1EventLength;
+    unsigned char  PM1ControlLength;
+    unsigned char  PM2ControlLength;
+    unsigned char  PMTimerLength;
+    unsigned char  GPE0Length;
+    unsigned char  GPE1Length;
+    unsigned char  GPE1Base;
+    unsigned char  CStateControl;
+    unsigned short WorstC2Latency;
+    unsigned short WorstC3Latency;
+    unsigned short FlushSize;
+    unsigned short FlushStride;
+    unsigned char  DutyOffset;
+    unsigned char  DutyWidth;
+    unsigned char  DayAlarm;
+    unsigned char  MonthAlarm;
+    unsigned char  Century;
+    unsigned short BootArchitectureFlags;
+    unsigned char  Reserved2;
+    unsigned int Flags;
+    GenericAddressStructure ResetReg;
+    unsigned char  ResetValue;
+    unsigned char  Reserved3[3];
+    unsigned int                X_FirmwareControl[2];
+    unsigned int                X_Dsdt[2];
+ 
+    GenericAddressStructure X_PM1aEventBlock;
+    GenericAddressStructure X_PM1bEventBlock;
+    GenericAddressStructure X_PM1aControlBlock;
+    GenericAddressStructure X_PM1bControlBlock;
+    GenericAddressStructure X_PM2ControlBlock;
+    GenericAddressStructure X_PMTimerBlock;
+    GenericAddressStructure X_GPE0Block;
+    GenericAddressStructure X_GPE1Block;
+};
